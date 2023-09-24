4c870ed384fc290df5f13360365756bc6bff683a
Christian Obed Arana navas <COANelmejor@gmail.com>
2023-09-24 17:42:55 -0600

    Formated SQL Code

--- 04-time/Challenge_2.sql
+++ 04-time/Challenge_2.sql
@@ -3,19 +3,15 @@ SELECT
 FROM
     platzi.alumnos
 WHERE
-    (
-        EXTRACT (
-            YEAR
-            FROM
-                fecha_incorporacion
-        ) = 2018
-    ) AND (
-        EXTRACT (
-            MONTH
-            FROM
-                fecha_incorporacion
-        ) = 5
-    )
+    EXTRACT (
+        YEAR
+        FROM
+            fecha_incorporacion
+    ) = 2018
+    AND EXTRACT (
+        MONTH
+        FROM
+            fecha_incorporacion
+    ) = 5
 ORDER BY
-    fecha_incorporacion ASC
-;
+    fecha_incorporacion ASC;
